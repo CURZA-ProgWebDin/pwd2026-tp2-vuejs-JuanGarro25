a6c9628b57d3b0da3fb8cbfbd9dd700973faa2ca
Juan Garro <comahue2025jg@gmail.com>
2026-05-06 19:15:28 -0300

    Add files via upload
    Trabajo Practico 2: Gestión de Productos (Vuejs)

---
+++ src/components/ProductItem.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+const props = defineProps({
+  producto: {
+    type: Object,
+    required: true
+  }
+})
+
+const emit = defineEmits(['borrar-producto'])
+
+function borrar() {
+  emit('borrar-producto', props.producto.id)
+}
+</script>
+
+<template>
+  <article class="tarjeta-producto">
+    <h3>{{ producto.nombre }}</h3>
+
+    <p>Categoría: {{ producto.categoria }}</p>
+    <p>Precio: ${{ producto.precio }}</p>
+    <p>Stock disponible: {{ producto.stock }}</p>
+    <p>Subtotal: ${{ producto.precio * producto.stock }}</p>
+
+    <button type="button" @click="borrar">
+      Quitar producto
+    </button>
+  </article>
+</template>
